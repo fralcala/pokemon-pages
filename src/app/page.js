889519a9ed15f80cfa989891ed05f075818a8453
fralcala <fralcala@iu.edu>
2024-10-27 22:01:12 -0400

    fixed nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,22 @@
-
+"use client";
+import usePokemonApi from "@/hooks/usePokemonApi";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  const pokeData = usePokemonApi();
+
+  useEffect(() => {
+    if (pokeData.totalPokemonCount === 0) {
+      pokeData.getNumberOfPokemon();
+    }
+  }, [pokeData]);
+
+  console.log(pokeData);
+
+  return (
+    <main className={styles.main}>
+      <h1>Home</h1>
+    </main>
+  );
 }
---
+++ src/hooks/usePokemonApi.js
@@ -0,0 +1,33 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const PokemonContext = createContext();
+
+export function PokemonProvider({ children }) {
+  const [pokemonState, setPokemonState] = useState({
+    totalPokemonCount: 0,
+  });
+
+  /**
+   * Fetches the pokemon api with a limit of 1 to minimize api call time. Uses count returned to determine the total number of pokemon stored on the api.
+   **/
+  async function getNumberOfPokemon() {
+    const pokeRequest = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=1`
+    );
+    const { count: pokemonCount } = await pokeRequest.json();
+    setPokemonState({ ...pokemonState, totalPokemonCount: pokemonCount });
+  }
+
+  const contextValue = { ...pokemonState, getNumberOfPokemon };
+
+  return (
+    <PokemonContext.Provider value={contextValue}>
+      {children}
+    </PokemonContext.Provider>
+  );
+}
+
+export default function usePokemon() {
+  return useContext(PokemonContext);
+}
